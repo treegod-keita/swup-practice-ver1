6c783fb58d90aaf49d0200ce0b4f1b4755812303
榊啓太 <treegod0205@gmail.com>
2026-06-07 16:47:13 +0900

    feat: swupによるページ遷移アニメーション実装

--- src/scripts/modules/animation.ts
+++ src/scripts/modules/animation.ts
@@ -1,15 +1,21 @@
 // animation.ts
-import { gsap } from "gsap";
+1;
 
 // ========================================================
 // [アウトアニメーション] 上にわずかに逃げながらフェードアウト
 // ========================================================
 export const outAnimation = () => {
+    document.documentElement.classList.add("is-wait");
     return gsap.to("#swup", {
-        y: -24,
         opacity: 0,
-        duration: 0.5,
-        ease: "power2.in",
+        y: -220,
+        ease: "power4.inOut",
+        duration: 1.4,
+        onStart: () => {
+            gsap.set("#swup", {
+                willChange: "transform",
+            });
+        },
     });
 };
 
@@ -19,17 +25,24 @@ export const outAnimation = () => {
 export const inAnimation = () => {
     window.scrollTo(0, 0);
 
-    return gsap.fromTo(
-        "#swup",
-        {
-            y: 32,
-            opacity: 0,
+    gsap.set("#swup", {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: 1,
+        willChange: "transform",
+    });
+    gsap.set("#swup", { y: "100%" });
+    return gsap.to("#swup", {
+        y: "0%",
+        ease: "power4.inOut",
+        duration: 1.4,
+        onComplete: () => {
+            gsap.to("[data-hide-element]", {
+                opacity: 1,
+                pointerEvents: "auto",
+            });
+            document.documentElement.classList.remove("is-wait");
         },
-        {
-            y: 0,
-            opacity: 1,
-            duration: 0.7,
-            ease: "power3.out",
-        },
-    );
+    });
 };
--- src/scripts/swup.ts
+++ src/scripts/swup.ts
@@ -1,21 +1,40 @@
 // swup.ts
 import Swup from "swup";
 import SwupBodyClassPlugin from "@swup/body-class-plugin";
-import { outAnimation, inAnimation } from "@/scripts/modules/animation";
+import { gsap } from "gsap";
+import SwupParallelPlugin from "@swup/parallel-plugin";
 
 const swup = new Swup({
     containers: ["#swup"], // Swupが差し替えるDOMの範囲
     animationSelector: false, // CSSアニメーションの完了待機を無効化
     animateHistoryBrowsing: true, // ブラウザの戻る・進むボタンを押したときもアニメーションを実行
-    plugins: [new SwupBodyClassPlugin()], // ページ遷移時に <body> のクラスを新しいページのものに自動で更新
+    plugins: [new SwupBodyClassPlugin(), new SwupParallelPlugin()], // ページ遷移時に <body> のクラスを新しいページのものに自動で更新
 });
 
-swup.hooks.replace("animation:out:await", async () => {
-    await outAnimation();
+swup.hooks.replace("animation:in:await", async (visit) => {
+    const previous = document.querySelector(".is-previous-container") as HTMLElement;
+    const next = document.querySelector("#swup:not(.is-previous-container)") as HTMLElement;
+
+    const scrollY = window.scrollY;
+    gsap.set(previous, { position: "absolute", top: scrollY, left: 0, width: "100%", zIndex: 0 });
+    gsap.set(next, { position: "absolute", top: 0, left: 0, width: "100%", y: "100%", zIndex: 1 });
+
+    await Promise.all([
+        gsap.fromTo(
+            previous,
+            { filter: "brightness(1)" },
+            { y: -200, filter: "brightness(0.3)", duration: 1.4, ease: "power4.inOut" },
+        ),
+        gsap.to(next, { y: "0%", duration: 1.4, ease: "power4.inOut" }),
+    ]);
+
+    gsap.set(next, { position: "", top: "", left: "", width: "", y: "", zIndex: "" });
+    window.scrollTo(0, 0); // アニメーション完了後にリセット
 });
 
-swup.hooks.replace("animation:in:await", async () => {
-    await inAnimation();
+swup.hooks.replace("content:scroll", () => {
+    return false;
+    // Swupの自動スクロールリセットを無効化
 });
 
 // // Astro用スクリプト再実行処理
